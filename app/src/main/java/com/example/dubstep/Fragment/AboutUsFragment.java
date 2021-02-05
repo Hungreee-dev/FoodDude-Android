6aa5dfe17a70f4353959b4a7d7dc7783b5167d1b
bibekpanda55 <rajendra21101999@gmail.com>
2021-02-05 20:13:44 +0530

    'ContactandAboutScreen'

--- app/src/main/java/com/example/dubstep/Fragment/AboutUsFragment.java
+++ app/src/main/java/com/example/dubstep/Fragment/AboutUsFragment.java
@@ -1,12 +1,11 @@
 package com.example.dubstep.Fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.dubstep.R;
 
@@ -17,12 +16,11 @@ import com.example.dubstep.R;
  */
 public class AboutUsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +28,8 @@ public class AboutUsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutUsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+
     public static AboutUsFragment newInstance(String param1, String param2) {
         AboutUsFragment fragment = new AboutUsFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/example/dubstep/Fragment/ContactUsFragment.java
+++ app/src/main/java/com/example/dubstep/Fragment/ContactUsFragment.java
@@ -1,12 +1,11 @@
 package com.example.dubstep.Fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.dubstep.R;
 
@@ -30,14 +29,7 @@ public class ContactUsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactUsFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static ContactUsFragment newInstance(String param1, String param2) {
         ContactUsFragment fragment = new ContactUsFragment();
